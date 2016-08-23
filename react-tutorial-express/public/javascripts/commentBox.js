2bd02150556ff44f65af1b46ef5823f80a1b1b08
YComputer <thea@163.com>
2016-08-23 15:32:38 +0800

    增加评论功能

--- react-tutorial-express/public/javascripts/commentBox.js
+++ react-tutorial-express/public/javascripts/commentBox.js
@@ -1,6 +1,4 @@
-
-
-
+// 评论模块
 var CommentBox = React.createClass({
     loadCommentsFromServer: function () {
         $.ajax({
@@ -9,6 +7,20 @@ var CommentBox = React.createClass({
             cache: false,
             success: function (data) {
                 console.log('data is', data);
+                this.setState({ data: data });
+            }.bind(this),
+            error: function (xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+    handleCommentSubmit: function (comment) {
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            type: 'POST',
+            data: comment,
+            success: function (data) {
                 this.setState({ data: data });
             }.bind(this),
             error: function (xhr, status, err) {
@@ -29,7 +41,7 @@ var CommentBox = React.createClass({
             <div className="commentBox">
                 <h1>Comments</h1>
                 <CommentList data={this.state.data} />
-                <CommentForm />
+                <CommentForm onCommentSubmit={this.handleCommentSubmit} />
             </div>
         );
     }
@@ -38,7 +50,7 @@ var CommentBox = React.createClass({
 
 var CommentList = React.createClass({
     render: function () {
-    console.log('this.props.data',typeof this.props.data);
+        console.log('this.props.data', typeof this.props.data);
 
         var commentNodes = this.props.data.map(function (comment) {
             return (
@@ -56,11 +68,46 @@ var CommentList = React.createClass({
 });
 
 var CommentForm = React.createClass({
+    getInitialState: function () {
+        return { author: '', text: '' };
+    },
+    handleAuthorChange: function (e) {
+        this.setState({ author: e.target.value });
+    },
+    handleTextChange: function (e) {
+        this.setState({ text: e.target.value });
+    },
+    handleSubmit: function (e) {
+        e.preventDefault();
+        var author = this.state.author.trim();
+        var text = this.state.text.trim();
+        if (!text || !author) {
+            return;
+        }
+        // TODO: send request to the server
+        this.props.onCommentSubmit({ author: author, text: text });
+        this.setState({ author: '', text: '' });
+    },
+
     render: function () {
         return (
-            <div className="commentForm">
-                Hello, world!I am a CommentForm.
-            </div>
+            <form className="commentForm" onSubmit={this.handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Your name"
+                    value={this.state.author}
+                    onChange={this.handleAuthorChange}
+                    />
+                <br/>
+                <input
+                    type="text"
+                    placeholder="Say something..."
+                    value={this.state.text}
+                    onChange={this.handleTextChange}
+                    />
+                <br/>
+                <input type="submit" value="Post" />
+            </form>
         );
     }
 });
--- react-tutorial-express/routes/index.js
+++ react-tutorial-express/routes/index.js
@@ -21,4 +21,31 @@ router.get('/api/comments', function (req, res, next) {
     res.json(JSON.parse(data));
   });
 });
+
+// post comments
+router.post('/api/comments', function (req, res, next) {
+  fs.readFile(COMMENTS_FILE, function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    var comments = JSON.parse(data);
+    // NOTE: In a real implementation, we would likely rely on a database or
+    // some other approach (e.g. UUIDs) to ensure a globally unique id. We'll
+    // treat Date.now() as unique-enough for our purposes.
+    var newComment = {
+      id: Date.now(),
+      author: req.body.author,
+      text: req.body.text,
+    };
+    comments.push(newComment);
+    fs.writeFile(COMMENTS_FILE, JSON.stringify(comments, null, 4), function(err) {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+      res.json(comments);
+    });
+  });
+});
 module.exports = router;
